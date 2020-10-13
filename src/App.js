3a29e875cb4547d2165e4c0866caaaf1e6e1d23d
Annemarie Thomas <Athomas9sa@users.noreply.github.com>
2020-10-12 23:12:06 -0400

    Redirect Branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import "bloomer";
 import IssueList from './components/IssueList';
+import Issue from './components/Issue';
+import IssueDetail from './components/IssueDetail';
 import "bulma/css/bulma.css";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-        <IssueList message="Hallo List" />
+    <Router>
+      <div className="App">
+        <nav>
+            <Link to='/'>IssueList</Link>
+            <Link to='/blog'>Issue</Link>
+            <Link to='/about'>IssueDetail</Link>
+        </nav>
       </div>
+        <Switch>
+              <Route exact path={`${url}/:id`}><IssueList />
+              </Route>
+              <Route path='/Issue'><Issue />
+              </Route>
+                <Route path='/IssueDetails'><IssueDetail />
+              </Route>
+              <Route path="*">
+                <Redirect to='/' />
+              </Route>
+        </Switch>
+    </Router>
   );
 }
 
